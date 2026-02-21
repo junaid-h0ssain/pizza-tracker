fccb188802f8e55a4b290e9f6b076e0e28406bb2
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-02-22 04:30:02 +0600

    admin user added

--- models/models.go
+++ models/models.go
@@ -7,25 +7,27 @@ import (
 	"gorm.io/gorm"
 )
 
-
-
 type DBModel struct {
+	DB    *gorm.DB
 	Order OrderModel
+	User  UserModel
 }
 
 func InitDB(dataSourceName string) (*DBModel, error) {
 	db, err := gorm.Open(sqlite.Open(dataSourceName), &gorm.Config{})
 	if err != nil {
-		return nil, fmt.Errorf("failed to connect to database: %w", err)
+		return nil, fmt.Errorf("failed to connect to database: %v", err)
 	}
 
-	err = db.AutoMigrate(&Order{}, &OrderItem{})
+	err = db.AutoMigrate(&Order{}, &OrderItem{}, &User{})
 	if err != nil {
-		return nil, fmt.Errorf("failed to migrate database: %w", err)
+		return nil, fmt.Errorf("failed to migrate database: %v", err)
 	}
 
 	dbModel := &DBModel{
+		DB:    db,
 		Order: OrderModel{DB: db},
+		User:  UserModel{DB: db},
 	}
 
 	return dbModel, nil
