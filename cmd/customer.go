e0796223df773074d6da1acb99b7c7e777e4ca2f
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-02-22 03:42:13 +0600

    upates to customer data

--- cmd/customer.go
+++ cmd/customer.go
@@ -8,6 +8,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type CustomerData struct {
+	Title    string
+	Order    models.Order
+	Statuses []string
+}
 type OrderFormData struct {
 	PizzaTypes []string
 	PizzaSizes []string
@@ -78,7 +83,9 @@ func (h *Handler) serveCustomer(c *gin.Context) {
 		return
 	}
 
-	c.HTML(http.StatusOK, "customer.tmpl", gin.H{
-		"Order": order,
+	c.HTML(http.StatusOK, "customer.tmpl", CustomerData{
+		Title:    "Pizza Order Status " + orderID,
+		Order:    *order,
+		Statuses: models.OrderStatuses,
 	})
 }
